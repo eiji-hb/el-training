1bbdbfc0b607d740fed566084129d3e94068631c
eiji-hb <eijisugiurar@gmail.com>
2020-06-07 20:23:56 +0900

    ステップ7-3: タスクを削除できるようにしましょう

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def new
-
+    @task = Task.new
   end
 
   def index
@@ -11,7 +11,34 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task), notice: '成功'
+    else
+      render :new
+    end
+  end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update!(task_params)
+    flash[:notice] = '更新しました。'
+    redirect_to root_path
+  end
+  def destroy
+    @task = Task.find(params[:id])
+    flash[:notice] = '消去しました。'
+    @task.destroy
+    redirect_to root_path
+  end
+  private
+
+  def task_params
+    params.require(:task).permit(:name,:description)
   end
 end
