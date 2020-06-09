c0848a36a2f086b1636516cb74fc7342c81fbe12
eiji-hb <eijisugiurar@gmail.com>
2020-06-09 13:47:19 +0900

    user_model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_141259) do
+ActiveRecord::Schema.define(version: 2020_06_09_044353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2020_06_08_141259) do
     t.datetime "deadline"
     t.string "status"
     t.integer "priority"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
